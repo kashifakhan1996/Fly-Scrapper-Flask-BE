4c505e921cc56350965d15c7d69aee16691f5f16
kashifakhan1996 <khankashifa.1996@gmail.com>
2025-07-04 10:41:58 +0530

    dotenv flask setup commit,updated requirement.txt pushed by static data due to slow api

--- run.py
+++ run.py
@@ -1,8 +1,14 @@
+import os
 from flask_cors import CORS
 from app import create_app
 
 app = create_app()
 CORS(app, resources={r"/api/*": {"origins": "http://localhost:5173"}})
 
+@app.route('/')
+def home():
+    return 'Hello from Flask on Render!'
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Fallback to 5000 if PORT is not set
+    app.run(host='0.0.0.0', port=port)
